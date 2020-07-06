ea95c0101dc66c9af041fd7d25fb43efaaa26fce
Nicolas Maillet <n.maillet07@gmail.com>
2020-07-06 17:20:45 +0200

    Start to add score & next piece features

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 // DOM's Element:
 const grid = document.querySelector('.grid');
+const scoreDisplay = document.querySelector('.score');
+const nextDisplay = document.querySelector('.next');
+
 
 // Place all cells and put them in a conts
 for(let i = 0; i <= 209; i++) {
@@ -19,6 +22,7 @@ for(let i = 200; i < 210; i++) {
 // Variables: 
 const lineWidth = 10; 
 
+const nextCells = document.querySelectorAll('.small-grid div');
 
 
 // The different shapes, and their 4 rotate
@@ -28,28 +32,24 @@ const lShape = [
     [1, lineWidth+1, lineWidth*2+1, lineWidth*2],
     [lineWidth, lineWidth*2, lineWidth*2+1, lineWidth*2+2]
 ]
-
 const zshape = [
     [0,lineWidth,lineWidth+1,lineWidth*2+1],
     [lineWidth+1, lineWidth+2,lineWidth*2,lineWidth*2+1],
     [0,lineWidth,lineWidth+1,lineWidth*2+1],
     [lineWidth+1, lineWidth+2,lineWidth*2,lineWidth*2+1]
 ]
-
 const tShape = [
     [1,lineWidth,lineWidth+1,lineWidth+2],
     [1,lineWidth+1,lineWidth+2,lineWidth*2+1],
     [lineWidth,lineWidth+1,lineWidth+2,lineWidth*2+1],
     [1,lineWidth,lineWidth+1,lineWidth*2+1]
 ]
-
 const oShape = [
     [0,1,lineWidth,lineWidth+1],
     [0,1,lineWidth,lineWidth+1],
     [0,1,lineWidth,lineWidth+1],
     [0,1,lineWidth,lineWidth+1]
 ]
-
 const ishape = [
     [1,lineWidth+1,lineWidth*2+1,lineWidth*3+1],
     [lineWidth,lineWidth+1,lineWidth+2,lineWidth+3],
@@ -59,14 +59,54 @@ const ishape = [
 
 const shapes = [lShape, zshape, tShape, oShape, ishape];
 
+
+const lShapeMini = [
+    [1, 4+1, 4*2+1, 2],
+    [4, 4+1, 4+2, 4*2+2],
+    [1, 4+1, 4*2+1, 4*2],
+    [4, 4*2, 4*2+1, 4*2+2]
+]
+const zshapeMini = [
+    [0,4,4+1,4*2+1],
+    [4+1, 4+2,4*2,4*2+1],
+    [0,4,4+1,4*2+1],
+    [4+1, 4+2,4*2,4*2+1]
+]
+const tShapeMini = [
+    [1,4,4+1,4+2],
+    [1,4+1,4+2,4*2+1],
+    [4,4+1,4+2,4*2+1],
+    [1,4,4+1,4*2+1]
+]
+const oShapeMini = [
+    [0,1,4,4+1],
+    [0,1,4,4+1],
+    [0,1,4,4+1],
+    [0,1,4,4+1]
+]
+const ishapeMini = [
+    [1,4+1,4*2+1,4*3+1],
+    [4,4+1,4+2,4+3],
+    [1,4+1,4*2+1,4*3+1],
+    [4,4+1,4+2,4+3]
+]
+
+const shapesMini = [lShapeMini, zshapeMini, tShapeMini, oShapeMini, ishapeMini];
+
+
 const colors = ['green', 'red', 'yellow', 'blue', 'orange'];
 
 let random = Math.floor(Math.random() * shapes.length);
+let randomNext = Math.floor(Math.random() * shapesMini.length);
 let currentPosition = 4;
 let currentRotation = 0;
+let indexRotate = 0; 
 let currentColor = colors[random];
-let current = shapes[random][0];
+let current = shapes[random][indexRotate];
+let nextShape = shapes[randomNext][indexRotate]; 
+let nextShapeMini = shapesMini[randomNext][indexRotate]; 
 
+let score = 0;
 
 const draw = () => {
     current.forEach(index => {
@@ -75,13 +115,16 @@ const draw = () => {
     })
 }
 
+nextShapeMini.forEach(index => {
+    nextCells[1 + index].classList.add('orange');
+})
+
 const undraw = () => {
     current.forEach(index => {
         cells[currentPosition + index].classList.remove('current-shape');
         cells[currentPosition + index].classList.remove(currentColor);
     })
 }
-
 
 const move = () => {
     
@@ -92,17 +135,27 @@ const move = () => {
     if(current.some(index => cells[currentPosition + lineWidth + index].classList.contains('taken'))) {
         current.forEach(index => cells[currentPosition + index].classList.add('taken'));
         random = Math.floor(Math.random() * shapes.length);
-        current = shapes[random][0];
+        current = shapes[randomNext][indexRotate];
         currentColor = colors[random];
         currentPosition = 4;    
         draw();
+        score += 10;
+        scoreDisplay.textContent = score;
     }
 }
 
 const moveLeft = () => {
+
+    if(currentPosition % lineWidth == 0) {
+        console.log('nop');
+        
+    }
+
     undraw();
     currentPosition -= 1;
     draw();
+
+    
 }
 
 const moveRight = () => {
@@ -115,8 +168,18 @@ const moveDown = () => {
     undraw();
     currentPosition += lineWidth;
     draw();
-}
 
+    if(current.some(index => cells[currentPosition + lineWidth + index].classList.contains('taken'))) {
+        current.forEach(index => cells[currentPosition + index].classList.add('taken'));
+        randomNext = Math.floor(Math.random() * shapes.length);
+        current = nextShape;
+        currentColor = colors[randomNext];
+        currentPosition = 4;    
+        draw();
+        score += 10;
+        scoreDisplay.textContent = score;
+    }
+}
 
 const pressKey = (event) => {
 
@@ -129,14 +192,26 @@ const pressKey = (event) => {
     if(event.keyCode == 40) {
         moveDown();
     }
+    if(event.keyCode == 32) {
+        undraw();
+
+        if(indexRotate == 3) {
+            indexRotate = 0;
+        } else {
+            indexRotate += 1;
+        }
+        
+        current = shapes[random][indexRotate];
+        draw();
+    }
 }
+
 
 draw();
 setInterval(move, 1000);
-
+scoreDisplay.textContent = score;
 
 
 document.addEventListener('keyup', pressKey);
-
 
 
